8632a513c7a5f95cd47844bf07486630f87ef22a
Ashish Dabral <ashishdabral2014@gmail.com>
2024-07-13 10:47:48 +0530

    Create In_search_of_an_easy_problem.cpp

---
+++ In_search_of_an_easy_problem.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+	int n;
+	cin>>n;
+	
+	int flag=0;
+	while(n--){
+		int problem;
+		cin>>problem;
+		
+		if(problem==1){
+			flag=1;
+		}
+	}
+	if(flag==0){
+		cout<<"EASY";
+	}else{
+		cout<<"HARD";
+	}
+
+}
